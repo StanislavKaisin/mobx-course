a18b3ae5d98bf3af6b76ca0d77ee6619abd06318
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2023-07-04 21:21:10 +0300

    draft, added factory

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
 import "./App.css";
 import { TodoInput } from "./Todo/TodoInput/TodoInput";
@@ -6,13 +7,23 @@ import { TodoInput } from "./Todo/TodoInput/TodoInput";
 import TodoStore from "./stores/TodoStore";
 import TodoList from "./Todo/TodoList/TodoList";
 
-const todos = new TodoStore();
+const todos = TodoStore();
 
 function App() {
+  const [todosVisible, setTodosVisible] = useState(true);
+  const handleClick = () => {
+    setTodosVisible((state) => !state);
+  };
   return (
     <div className="App">
       <TodoInput />
-      <TodoList />
+      <div className={styles["todo-list-wrapper"]}>
+        <h2 onClick={handleClick}>
+          <span>{todosVisible ? "-" : "+"}</span>
+          Todos
+        </h2>
+        {todosVisible && <TodoList />}
+      </div>
     </div>
   );
 }
--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -12,32 +12,48 @@ export interface ITodo {
   isDone: boolean;
 }
 
-class TodoStore {
-  list: ITodo[] = [];
-  constructor() {
-    // makeObservable(this, {
-    //   list: observable,
-    //   add: action,
-    //   toggle: action,
-    //   remove: action,
-    // });
-    makeAutoObservable(this, {
-      //not an action
-      // add: false,
-      //redefine function type
-      // add: computed,
-    });
-  }
-  add(title: string) {
-    if (title.length < 3) return;
-    this.list.push({ title, id: Date.now(), isDone: false });
-  }
-  toggle(todo: ITodo) {
-    todo.isDone = !todo.isDone;
-  }
-  remove(id: number) {
-    this.list = this.list.filter((item) => item.id !== id);
-  }
-}
+const TodoStore = () => {
+  return makeAutoObservable({
+    list: [] as ITodo[],
+    add(title: string) {
+      if (title.length < 3) return;
+      this.list.push({ title, id: Date.now(), isDone: false });
+    },
+    toggle(todo: ITodo) {
+      todo.isDone = !todo.isDone;
+    },
+    remove(id: number) {
+      this.list = this.list.filter((item) => item.id !== id);
+    },
+  });
+};
+
+// class TodoStore {
+//   list: ITodo[] = [];
+//   constructor() {
+//     // makeObservable(this, {
+//     //   list: observable,
+//     //   add: action,
+//     //   toggle: action,
+//     //   remove: action,
+//     // });
+//     makeAutoObservable(this, {
+//       //not an action
+//       // add: false,
+//       //redefine function type
+//       // add: computed,
+//     });
+//   }
+//   add(title: string) {
+//     if (title.length < 3) return;
+//     this.list.push({ title, id: Date.now(), isDone: false });
+//   }
+//   toggle(todo: ITodo) {
+//     todo.isDone = !todo.isDone;
+//   }
+//   remove(id: number) {
+//     this.list = this.list.filter((item) => item.id !== id);
+//   }
+// }
 
 export default TodoStore;
