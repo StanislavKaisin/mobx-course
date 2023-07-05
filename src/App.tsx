6d41964b03fc86fe4c7612059889d86f59703a7f
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2023-07-05 07:43:49 +0300

    added observable

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,35 @@ import { TodoInput } from "./Todo/TodoInput/TodoInput";
 
 import TodoStore from "./stores/TodoStore";
 import TodoList from "./Todo/TodoList/TodoList";
+import { observable } from "mobx";
+import { observer } from "mobx-react-lite";
 
 const todos = TodoStore();
 
 function App() {
-  const [todosVisible, setTodosVisible] = useState(true);
+  const [appUI] = useState(() => {
+    return observable({
+      todosVisible: true,
+      toggleTodoVisibility() {
+        this.todosVisible = !this.todosVisible;
+      },
+    });
+  });
   const handleClick = () => {
-    setTodosVisible((state) => !state);
+    appUI.toggleTodoVisibility();
   };
   return (
     <div className="App">
       <TodoInput />
       <div className={styles["todo-list-wrapper"]}>
         <h2 onClick={handleClick}>
-          <span>{todosVisible ? "-" : "+"}</span>
+          <span>{appUI.todosVisible ? "-" : "+"}</span>
           Todos
         </h2>
-        {todosVisible && <TodoList />}
+        {appUI.todosVisible && <TodoList />}
       </div>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
