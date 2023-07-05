e5883cd91453fec1f087a7445e9657efd52b8d15
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2023-07-05 15:15:41 +0300

    added local observable

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,33 @@ import { TodoInput } from "./Todo/TodoInput/TodoInput";
 import TodoStore from "./stores/TodoStore";
 import TodoList from "./Todo/TodoList/TodoList";
 import { observable } from "mobx";
-import { observer } from "mobx-react-lite";
+import { observer, useLocalObservable } from "mobx-react-lite";
 
 const todos = TodoStore();
 
 function App() {
-  const [appUI] = useState(() => {
-    return observable({
+  const appUI = useLocalObservable(() => {
+    return {
       todosVisible: true,
       toggleTodoVisibility() {
-        this.todosVisible = !this.todosVisible;
+        appUI.todosVisible = !appUI.todosVisible;
       },
-    });
+    };
   });
-  const handleClick = () => {
-    appUI.toggleTodoVisibility();
-  };
+  // const handleClick = () => {
+  //   appUI.toggleTodoVisibility();
+  // };
+  const todosVisible = observable.box(true);
+  //@ts-ignore
+  todosVisible.observe_((newValue) => {
+    console.log("newValue :>> ", newValue);
+  });
+  todosVisible.set(false);
   return (
     <div className="App">
       <TodoInput />
       <div className={styles["todo-list-wrapper"]}>
-        <h2 onClick={handleClick}>
+        <h2 onClick={appUI.toggleTodoVisibility}>
           <span>{appUI.todosVisible ? "-" : "+"}</span>
           Todos
         </h2>
