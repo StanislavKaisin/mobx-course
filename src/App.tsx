0ab589c3793e3d4e3bf82fc8a6072fe2d37ac380
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2023-07-09 07:39:21 +0300

    when reaction

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { TodoInput } from "./Todo/TodoInput/TodoInput";
 
 import TodoStore from "./stores/TodoStore";
 import TodoList from "./Todo/TodoList/TodoList";
-import { action, autorun, observable, reaction, runInAction } from "mobx";
+import { action, autorun, observable, reaction, runInAction, when } from "mobx";
 import { observer, useLocalObservable } from "mobx-react-lite";
 import { useStore } from "./stores";
 
@@ -28,26 +28,49 @@ function App() {
   //   };
   // }, []);
 
+  // useEffect(() => {
+  //   const disposedReaction = reaction(
+  //     () => {
+  //       return { length: todos.list.length, unfinished: todos.unfinishedTodos };
+  //     },
+  //     // ({ length, unfinished }) => {
+  //     //   console.log("length :>> ", length);
+  //     //   console.log("unfinished :>> ", unfinished);
+  //     //   throw new Error("custom error");
+  //     // },
+  //     (newValue, oldValue) => {
+  //       console.log("newValue :>> ", newValue);
+  //       console.log("oldValue :>> ", oldValue);
+  //       throw new Error("custom error");
+  //     },
+  //     { delay: 1000, onError: (err) => console.log(err) }
+  //   );
+  //   return () => {
+  //     disposedReaction();
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const disposeWhen = when(
+  //     () => {
+  //       return !appUI.todosVisible;
+  //     },
+  //     () => {
+  //       console.log("clean up");
+  //     }
+  //   );
+  //   return () => {
+  //     disposeWhen();
+  //   };
+  // }, []);
+
   useEffect(() => {
-    const disposedReaction = reaction(
-      () => {
-        return { length: todos.list.length, unfinished: todos.unfinishedTodos };
-      },
-      // ({ length, unfinished }) => {
-      //   console.log("length :>> ", length);
-      //   console.log("unfinished :>> ", unfinished);
-      //   throw new Error("custom error");
-      // },
-      (newValue, oldValue) => {
-        console.log("newValue :>> ", newValue);
-        console.log("oldValue :>> ", oldValue);
-        throw new Error("custom error");
-      },
-      { delay: 1000, onError: (err) => console.log(err) }
-    );
-    return () => {
-      disposedReaction();
-    };
+    const promiseWhen = when(() => {
+      return !appUI.todosVisible;
+    });
+    promiseWhen.then(() => {
+      console.log("clean up");
+    });
   }, []);
 
   const appUI = useLocalObservable(() => {
